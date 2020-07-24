f69986ce639ff7a60fc1b20f8386d13f8bf75b08
dpwdec <declankolakowski@gmail.com>
2020-07-24 12:24:01 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new System.Exception("Trig");
+            // var account = new BankAccount("Dec", 1000);
+            // Console.WriteLine($"Balance: {account.Balance}\nName: {account.Owner}\nIdent: {account.Number}\nAge:{account.Age}");
+            // var account2 = new BankAccount("Marcus", 200);
+            // Console.WriteLine($"Balance: {account2.Balance}\nName: {account2.Owner}\nIdent: {account2.Number}");
+            // var names = new List<string> { "<name>", "Ana", "Felipe" };
+
+            // Func<int, int> add = x => x + 1;
+            // Func<int, int> dub = x => x * 2;
+
+            // var g = 2;
+            // Console.WriteLine(dub(add(g)));
+            // Console.WriteLine(g);
+        }
+    }
+}
