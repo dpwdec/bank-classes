de4998eda32691b50128304fd16a7c81d5c2211b
dpwdec <declankolakowski@gmail.com>
2020-07-24 12:58:14 +0100

    Added negative balance check and cleaned code

--- Program.cs
+++ Program.cs
@@ -9,15 +9,15 @@ namespace classes
         {
             var account = new BankAccount("Dec", 100);
 
-            // try
-            // {
-            //     var account2 = new BankAccount("Dan", -55);
-            // }
-            // catch (ArgumentOutOfRangeException e)
-            // {
-            //     Console.WriteLine("Negative Balance");
-            //     Console.WriteLine(e.ToString());
-            // }
+            try
+            {
+                var account2 = new BankAccount("Dan", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Negative Balance 💸");
+                Console.WriteLine(e.ToString());
+            }
 
             try
             {
@@ -28,17 +28,6 @@ namespace classes
                 Console.WriteLine("Overdraft detected! ❌");
                 Console.WriteLine(e.ToString());
             }
-            // Console.WriteLine($"Balance: {account.Balance}\nName: {account.Owner}\nIdent: {account.Number}\nAge:{account.Age}");
-            // var account2 = new BankAccount("Marcus", 200);
-            // Console.WriteLine($"Balance: {account2.Balance}\nName: {account2.Owner}\nIdent: {account2.Number}");
-            // var names = new List<string> { "<name>", "Ana", "Felipe" };
-
-            // Func<int, int> add = x => x + 1;
-            // Func<int, int> dub = x => x * 2;
-
-            // var g = 2;
-            // Console.WriteLine(dub(add(g)));
-            // Console.WriteLine(g);
         }
     }
 }
